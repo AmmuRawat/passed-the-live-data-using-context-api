e8d0364bb85de125f6807fe10aff05fb92c0b66c
manishtomar <tomer.manish201298@gmail.com>
2023-12-28 16:46:44 +0530

    implement the setTime out

--- src/context/MyContextProvider.js
+++ src/context/MyContextProvider.js
@@ -2,25 +2,30 @@ import React, { useEffect, useState } from 'react';
 import MyContext from './MyConext';
 
 const MyContextProvider = ({ children }) => {
-const [data, setData] = useState([]) 
-console.log("data value is here ",data)
- 
-const getData = async () => {
-const res = await fetch("https://jsonplaceholder.typicode.com/posts/")
-const finaldata = await res.json()
-console.log("finaldata", finaldata)
-setData(finaldata)
-}  
- 
- 
-useEffect(()=>{
-   getData()
-    },[])
+  const [data, setData] = useState([]);
 
-  return <MyContext.Provider value={{data}}>
-    {children}
-    </MyContext.Provider>;
+  const getData = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts/");
+    const finaldata = await res.json();
+    console.log("finaldata", finaldata);
+    setData(finaldata);
+  };
+
+  useEffect(() => {
+    getData();
+    const intervalId = setInterval(() => {
+      getData();
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []); 
+
+  return (
+    <MyContext.Provider value={{ data }}>
+      {children}
+    </MyContext.Provider>
+  );
 };
 
 export default MyContextProvider;
+
 
